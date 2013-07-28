e4b4d56da69ca244204c5545bdcaaed08229f9a9
Steve Faulkner <southpolesteve@gmail.com>
2013-07-28 15:10:54 -0500

    Add deploy button image

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -1,6 +1,13 @@
 class DeployController < ApplicationController
   def show
-    app = current_user.apps.where(owner: params[:owner], name: params[:name]).first_or_create
-    redirect_to app
+    respond_to do |format|
+      format.html do
+        if current_user
+          app = current_user.apps.where(owner: params[:owner], name: params[:name]).first_or_create
+          redirect_to app
+        end
+      end
+      format.png { send_file view_context.image_path('deploy_button.png'), type: "image/png", disposition: "inline" }
+    end
   end
 end
