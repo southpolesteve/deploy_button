e308aa9239f2b7d35a3210f5ae4a3c66ce14e025
Steve Faulkner <southpolesteve@gmail.com>
2013-08-01 22:28:06 -0500

    Whoops. Left a binding.pry in

--- spec/features/requesting_a_deploy_spec.rb
+++ spec/features/requesting_a_deploy_spec.rb
@@ -9,7 +9,6 @@ describe 'User requests a deploy' do
 
   it "queues a new app for deployment" do
     visit "/deploy/begriffs/lucre"
-    binding.pry
     click_on 'OK! Go!'
     page.should have_content("deploying a new instance of begriffs/lucre")
   end
