e279e0544f695e10286d082b5dbb7490eae56f15
Steve Faulkner <southpolesteve@gmail.com>
2013-08-04 18:49:58 -0500

    Tests passing

--- spec/models/heroku_bot_spec.rb
+++ spec/models/heroku_bot_spec.rb
@@ -5,35 +5,35 @@ describe HerokuBot do
 
   describe 'account' do
     it 'calls the correct HTTParty method' do
-      HerokuBot.should_receive(:get).with('/account')
+      HerokuBot.should_receive(:get).with('/account').and_return(double(success?: true))
       HerokuBot.account
     end
   end
 
   describe 'transfer' do
     it 'calls the correct HTTParty method' do
-      HerokuBot.should_receive(:post).with('/account/app-transfers', an_instance_of(Hash))
+      HerokuBot.should_receive(:post).with('/account/app-transfers', an_instance_of(Hash)).and_return(double(success?: true))
       HerokuBot.transfer(deploy)
     end
   end
 
   describe 'add_user_as_collaborator' do
     it 'calls the correct HTTParty method' do
-      HerokuBot.should_receive(:post).with('/apps/afternoon-brook-7719/collaborators', an_instance_of(Hash))
+      HerokuBot.should_receive(:post).with('/apps/afternoon-brook-7719/collaborators', an_instance_of(Hash)).and_return(double(success?: true))
       HerokuBot.add_user_as_collaborator(deploy)
     end
   end
 
   describe 'remove_bot' do
     it 'calls the correct HTTParty method' do
-      HerokuBot.should_receive(:delete).with('/apps/afternoon-brook-7719/collaborators/test@example.com')
+      HerokuBot.should_receive(:delete).with('/apps/afternoon-brook-7719/collaborators/test%40example.com').and_return(double(success?: true))
       HerokuBot.remove_bot(deploy)
     end
   end
 
   describe 'create' do
     it 'calls the correct HTTParty method' do
-      HerokuBot.should_receive(:post).with('/apps')
+      HerokuBot.should_receive(:post).with('/apps').and_return(double(success?: true))
       HerokuBot.create
     end
   end
