8dfb1b7abd3a819b552a3f755e5291be0d29f61b
Steve Faulkner <southpolesteve@gmail.com>
2013-08-11 13:00:21 -0500

    Change up sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,22 +1,31 @@
 require 'sidekiq/web'
+require 'autoscaler/sidekiq'
+require 'autoscaler/heroku_scaler'
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == [ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']]
 end
 
-if Rails.env.production?
-  require 'autoscaler/sidekiq'
-  require 'autoscaler/heroku_scaler'
+heroku = nil
+if ENV['HEROKU_APP']
+  heroku = Autoscaler::HerokuScaler.new
+end
 
-  Sidekiq.configure_client do |config|
+Sidekiq.configure_client do |config|
+  if heroku
     config.client_middleware do |chain|
-      chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new
+      chain.add Autoscaler::Sidekiq::Client, 'default' => heroku
     end
   end
+end
 
-  Sidekiq.configure_server do |config|
-    config.server_middleware do |chain|
-      chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new, 60)
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    if heroku
+      p "[Sidekiq] Running on Heroku, autoscaler is used"
+      chain.add(Autoscaler::Sidekiq::Server, heroku, 60) # 60 seconds timeout
+    else
+      p "[Sidekiq] Running locally, so autoscaler isn't used"
     end
   end
 end
